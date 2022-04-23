46aa9983acfe151b820ed442739d849c005b8873
EgorAdonev <EgorAdonev@github.com>
2022-04-24 02:08:18 +0300

    developing parsing(pretty print)

--- src/main/java/org/adonev/moviesearch/MovieSearch.java
+++ src/main/java/org/adonev/moviesearch/MovieSearch.java
@@ -8,15 +8,17 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
-import static org.asynchttpclient.Dsl.asyncHttpClient;
-
-
-public class MovieSearch {
+public class MovieSearch extends Logger {
     private static String movieName;
     private static String status;
     private static String urlEncodedStr;
+    public static Logger LOG = Logger.getLogger("MovieSearch");
     public MovieSearch(){
+        super("MovieSearch","org.adonev.moviesearch");
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,62 +42,64 @@ public class MovieSearch {
         String info = movie.attr("class","info").text();
         String[] infoAttrs = info.split(" ");
 
-//        name = infoAttrs[0];
-        String infoAttrsString = null;
-        for (int i = 0 ; i < infoAttrs.length; i++){
-            char[] movieAttr = new char[infoAttrs[0].length()];
-            movieAttr = infoAttrs[0].toCharArray();
-            infoAttrsString = String.valueOf(movieAttr);
+        String infoAttrsString = "";
+        for (int i = 0 ; i < infoAttrs.length - 1; i++){
+            String movieAttr = infoAttrs[0];
+            infoAttrsString = movieAttr;
+            infoAttrsString += "\n";
 
-            char[] movieNameAttr = new char[infoAttrs[1].length()];
-            movieNameAttr = infoAttrs[1].toCharArray();
-            infoAttrsString+=String.valueOf(movieNameAttr);
+            String movieNameAttr = infoAttrs[1];
+            infoAttrsString += movieNameAttr;
+            infoAttrsString += "\n";
 
-            char[] movieYearAttr = new char[infoAttrs[2].length()];
-            movieNameAttr = infoAttrs[2].toCharArray();
+            char[] movieYearAttr = infoAttrs[2].toCharArray();
             infoAttrsString+=String.valueOf(movieYearAttr);
-
-            if(infoAttrs[i]=="Directed"){
-                infoAttrsString+=infoAttrs[i] + infoAttrs[i+1];
-                if(infoAttrs[i]=="/")
-                {
-                    infoAttrsString+=infoAttrs[i];
-                    if(infoAttrs[i]=="Genres:") {
-                        infoAttrsString+=infoAttrs[i];
-                        break;
+            infoAttrsString += "\n";
+            if(infoAttrs[i].equals("Directed")) {
+                String movieDirected = infoAttrs[i];
+                if (!infoAttrs[i].equals("Movie")) {
+                    String movieBy = infoAttrs[i+1];//spider
+                    infoAttrsString += movieDirected + movieBy;
+                    infoAttrsString += "\n";
+                    if (infoAttrs[i] == "/") {
+                        infoAttrsString += infoAttrs[i] + infoAttrs[i + 1];
+                        if (infoAttrs[i] == "Genres:") {
+                            infoAttrsString += infoAttrs[i];
+//                        break;
+                        }
                     }
-                    infoAttrsString+=infoAttrs[i];
-                    if(infoAttrs[i]=="/") {
-                        infoAttrsString += infoAttrs[i];
-                    }
-                } else {
-                    break;
+                    infoAttrsString += infoAttrs[i];
                 }
-
-                char[] movieDirectorsAttr = new char[8+3+infoAttrs[i+2].length()+infoAttrs[i+3].length()];
-                movieDirectorsAttr = infoAttrs[2].toCharArray();
-                infoAttrsString += String.valueOf(movieDirectorsAttr);
             }
-        }
-        StringBuilder name = null;
-        String year;
-        StringBuilder directors = null;
-        StringBuilder genres = null;
-        switch (infoAttrsString){
-            case "Movie":
-                name = name.append("\n").append(infoAttrs[1]);
-                //continue;
-            case "Directed by:":
-                directors = directors.append("\n").append(infoAttrs[1]);
-            case "Genres:":
-
+                else {
+                    continue;
+                }
+//                char[] movieDirectorsAttr = new char[8+3+infoAttrs[i+2].length()+infoAttrs[i+3].length()];
+//                movieDirectorsAttr = infoAttrs[2].toCharArray();
+//                infoAttrsString += String.valueOf(movieDirectorsAttr);
+//            }
+//            else {
+//                continue;
+//            }
 
         }
-        for (int i = 0; i < infoAttrs.length; i++){
-            if (infoAttrs[i].equals(" ")) infoAttrsString += "\n"{
-
-            }
-        }
+        System.out.println(infoAttrsString);
+        LOG.log(Level.INFO,info);
+//        StringBuilder name = null;
+//        String year;
+//        StringBuilder directors = null;
+//        StringBuilder genres = null;
+//        switch (info){
+//            case "Movie":
+//                name = name.append("\n").append(infoAttrs[1]);
+//                //continue;
+//            case "Directed by:":
+//                directors = directors.append("\n").append(infoAttrs[6]).append(infoAttrs[7]);
+//            case "Genres:":
+//        }
+//        for (int i = 0; i < infoAttrs.length - 1; i++){
+//
+//        }
         Element firstMovieByQuery = searchResults.getElementsByAttributeValue("class","movie").first();
         String infoOfFirstMovie = firstMovieByQuery.attr("class","info").text();//attr("class","info")
         System.out.println(infoOfFirstMovie);
