9eec059b1aa43e71b32dd5e8b99587a8a0e9d8fc
EgorAdonev <EgorAdonev@github.com>
2022-05-02 22:35:31 +0300

    refactor, added final fields

--- src/main/java/org/adonev/moviesearch/MovieSearch.java
+++ src/main/java/org/adonev/moviesearch/MovieSearch.java
@@ -14,38 +14,41 @@ import java.util.logging.Level;
 import static org.adonev.moviesearch.CustomLogger.LOG;
 
 public class MovieSearch {
+    private static final String movieSiteURL = "https://www.allmovie.com/search/all/";
+    private static final String userAgent = "Chrome";
     private static String movieName;
     private static String status;
-    private static String urlEncodedStr;
+    private static String urlEncodedMovieName;
+
 
     public static void main(String[] args) throws IOException {
-        //TODO input URL
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter movie name: ");
         movieName = scanner.nextLine();
-        String urlEncodedStr=URLEncoder.encode(movieName, StandardCharsets.UTF_8.name());
-        if(movieName==null) {
+        urlEncodedMovieName=URLEncoder.encode(movieName, StandardCharsets.UTF_8.name());
+        if(movieName==null || movieName.isEmpty()) {
             System.out.println("Movie name cannot be empty!");
         }
 
-        Document docConn = Jsoup.connect("https://www.allmovie.com/search/all/"+urlEncodedStr)
-                .userAgent("Chrome")
+        Document docConnection = Jsoup.connect(movieSiteURL+urlEncodedMovieName)
+                .userAgent(userAgent)
                 .timeout(5000)
                 .get();
-        System.out.format("Current site: https://www.allmovie.com/search/all/%s\n",urlEncodedStr);
-        Element searchResults = docConn.select("#cmn_wrap > div.content-container > div.content > div > ul").first();
+        System.out.format("Current site: %s,%s\n",movieSiteURL,urlEncodedMovieName);
+        Element searchResults = docConnection.select("#cmn_wrap > div.content-container > div.content > div > ul").first();
         Elements movie = searchResults.children().attr("class","movie");
         String info = movie.attr("class","info").text();
 
         String[] infoAttrs = info.split(" ");
         String[] infoAttr = info.split("\\d{4}");
-        String str =  info.replaceAll("Movie","\nMovie\n")
+        String prettyPrintInfo =  info.replaceAll("Movie","\nMovie\n")
                 .replaceAll("Person","Person\n")
                 .replaceAll("Active:","Active\n")
                 .replaceAll("Directed by:","\nDirected by:\n")
                 .replaceAll("/","\n")
                 .replaceAll("Genres:","\nGenres:\n");
-        System.out.println(str);
+        System.out.println(prettyPrintInfo);
         LOG.log(Level.INFO,info);
     }
 }
